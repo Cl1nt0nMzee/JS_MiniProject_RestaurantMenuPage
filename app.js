c1ffeedacf1376ad71e604b4b1c26a1c3befc8bc
Clinton Mzee <clinton.mzee@outlook.com>
2023-05-24 19:38:59 +0200

    Initalize Project Skeleton

---
+++ app.js
@@ -0,0 +1,28 @@
+const menuItems = [
+    {
+        id: 1,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "",
+        des: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod"
+    },
+    {
+        id: 2,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "",
+        des: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod"
+
+    },
+    {
+        id: 3,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "",
+        des: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod"
+
+    }
+]
